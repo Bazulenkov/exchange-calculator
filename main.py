d4a226fdbcc3551a2aae18eac83b7fe992485f08
Alexey Potapov <2furius@gmail.com>
2022-11-05 21:45:06 +0300

    Review

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ def main():
     logger = logging.getLogger(__name__)
     logger.debug("Bot started")
 
+    # в качестве улучшения можно еще чекать, что токены реально были.
+    # пустые строки по умолчанию для токеном в запаре могут сломать мозги :)
+    # лучше проверить, что токены не пустые, и если что просто остановится исключением или sys.exit
     load_dotenv()
     telegram_token = os.getenv("TELEGRAM_TOKEN", "")
     chat_id = os.getenv("TELEGRAM_CHAT_ID", "")
     args = (telegram_token, chat_id)
     binance_client = BinanceClient()
-    message = "{symbol} = {price}"
+    message = "{symbol} = {price}"  # тут и ниже -- это ж настройки. Не хочешь утащить их в файл настроек или на худой конец в константы наверх?
     symbol = "BTCUSDT"
     while True:
         try:
@@ -28,7 +31,7 @@ def main():
         except Exception as e:
             error_message = f" Сбой в работе программы: {e}"
             send_message(error_message, *args)
-        time.sleep(1 * 60)
+        time.sleep(1 * 60)  # а тут в число учебных целях можно показать finally.
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -19,9 +19,16 @@ def send_message(message: str, tlgm_token, chat_id):
     """
     try:
         bot = telegram.Bot(token=tlgm_token)
-        posted_message = bot.send_message(chat_id=chat_id, text=message)
-        logger.info(f'Message has sent to Telegram: "{message}"')
+        posted_message = bot.send_message(
+            chat_id=chat_id, text=message
+        )  # не увидел, что оно используется :)
+        logger.info(
+            f'Message has sent to Telegram: "{message}"'
+        )  # число в учебных целях можно рассказать про else
         return posted_message
     except telegram.error.TelegramError as e:
         logger.error(e)
-        raise e
+        raise e  # у нас же зависимость от библиотеки telegram только в этом файле?
+    # Если да, то тут точно будет удачнее кинуть свое исключение через raise ... from ..., особенно, если нужно как-то отдельно реагировать на эту ситуацию
+    # А тут та самая ситуация. Если функция плюнула исключение, то в main будет провал в блок except Exception,
+    # в котором снова попробуем отправить сообщение. При неудаче можно сложить стек =)
