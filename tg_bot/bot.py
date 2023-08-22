47a54c527fbae88cd6f3a5940f214c6e7cf99656
Bazulenkov <anton@bazul.ru>
2023-08-22 17:12:42 +0400

    feat: Added tg_bot.

---
+++ tg_bot/bot.py
@@ -0,0 +1,29 @@
+import asyncio
+from distutils.cmd import Command
+
+from aiogram import Bot, Dispatcher
+
+from configs import configure_logging
+from handlers import questions, different_types
+
+
+async def main():
+    bot = Bot(token="TOKEN")
+    dp = Dispatcher()
+
+    dp.include_routers(questions.router, different_types.router)
+
+    # Альтернативный вариант регистрации роутеров по одному на строку
+    # dp.include_router(questions.router)
+    # dp.include_router(different_types.router)
+
+    # Запускаем бота и пропускаем все накопленные входящие
+    # Да, этот метод можно вызвать даже если у вас поллинг
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    configure_logging("tg_bot.log")
+
+    asyncio.run(main())
